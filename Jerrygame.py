27383b1acd710352b70767ab60c20f9add325a25
jerryyx <yao.gang6@outlook.com>
2017-06-10 18:11:21 +0800

    initial

---
+++ Jerrygame.py
@@ -0,0 +1,9 @@
+from tkinter import *
+import random
+import time
+
+class Game:
+    def __init__(self):
+        self.tk = Tk()
+        self.tk.title("Mr.Stick Man Races for the Exit")
+        self.tk.resizable(0, 0)
